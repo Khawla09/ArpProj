c06b74335b31129246fe0d321adb5012435aa7a8
Arpana <muriki.arpana@gmail.com>
2022-08-09 17:46:08 +0530

    Third Commit

--- src/main/java/Sce01/TestCase3.java
+++ src/main/java/Sce01/TestCase3.java
@@ -1,10 +1,10 @@
 package Sce01;
 
-public class TestCase2 {
+public class TestCase3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is my Test case 2");
+
 	}
 
 }
